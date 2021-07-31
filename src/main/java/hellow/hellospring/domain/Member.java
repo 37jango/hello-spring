d79db976a9592ec88eb3e33c3793156aba0ee2ed
jango <789456jang@naver.com>
2021-08-01 03:57:22 +0900

    Make a Repository

--- src/main/java/hellow/hellospring/domain/Member.java
+++ src/main/java/hellow/hellospring/domain/Member.java
@@ -1,27 +1,23 @@
 package hellow.hellospring.domain;
 
-/*
-데이터 : 회원ID, 이름
-기능: 회원 등록, 조회
-아직 데이터 저장소가 선정되지 않음, DB가 아직 선정X(가상 시나리오)
- */
 public class Member {
-	private Long id;
-	private String name;
 
-	public Long getId() {
-		return id;
-	}
+    private Long id;
+    private String name;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/hellow/hellospring/repository/MemberRepository.java
+++ src/main/java/hellow/hellospring/repository/MemberRepository.java
@@ -6,9 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-	Member save(Member member);
-	Optional<Member> findById(Long id);
-	Optional<Member> findByName(String name);
-	List<Member> findAll();
-	//Optional? -> Null이 반환되지 않게 한 번 더 감싸주는 방식
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
 }
