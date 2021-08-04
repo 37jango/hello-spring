537a364ea914a0c6d26aa65d05dd2cc7b072bb19
jango <789456jang@naver.com>
2021-08-05 03:24:33 +0900

    Spring JDBC

--- src/main/java/hellow/hellospring/SpringConfig.java
+++ src/main/java/hellow/hellospring/SpringConfig.java
@@ -1,13 +1,28 @@
 package hellow.hellospring;
 
+import hellow.hellospring.repository.JdbcMemberRepository;
 import hellow.hellospring.repository.MemberRepository;
-import hellow.hellospring.repository.MemoryMemberRepository;
 import hellow.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    @Autowired
+    DataSource dataSource;
+
+    // or
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +31,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
